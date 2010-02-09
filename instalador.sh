576104d3638c6d2464b7740b47d0fda0c5d52f98
Jayson Reis <santosdosreis@gmail.com>
2010-02-09 17:17:28 -0200

    Corre??es

--- instalador.sh
+++ instalador.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
-gksu -c "cp $PWD/gerador-de-documentos-nacionais.py /usr/bin/ && chmod +x /usr/bin/gerador-de-documentos-nacionais.py"
-gksu -c "cp $PWD/GNOME_GeradorDeDocumentosNacionaisApplet.server /usr/lib/bonobo/servers/"
+if [ $(whoami) != "root" ]
+then
+    echo "Executando o instalador como root"
+    gksu "$(readlink -f "$0")"
+    exit 0
+fi
 
+zenity --question --text="Você tem certeza que deseja instalar o applet de geração de documentos nacionais ?"
+if [ $? -ne 0 ]
+then
+    zenity --info --text="Instalação abortada."
+    exit 1
+fi
+
+cp $PWD/gerador-de-documentos-nacionais.py /usr/bin/ && chmod +x /usr/bin/gerador-de-documentos-nacionais.py
+cp $PWD/GNOME_GeradorDeDocumentosNacionaisApplet.server /usr/lib/bonobo/servers/
+
+if [ $? -eq 0 ]
+then
+    zenity --info --text="Applet instalado com sucesso"
+    exit 0
+fi
